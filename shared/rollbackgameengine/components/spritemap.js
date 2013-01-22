374d4531fa9fad0540699a5a6bd34cde2a4aa35c
N A <MusicSales@Ns-iMac.local>
2013-01-22 10:10:06 -0500

    Fixed spritemap bug where 1x animations wouldn't work ever again

--- shared/rollbackgameengine/components/spritemap.js
+++ shared/rollbackgameengine/components/spritemap.js
@@ -87,6 +87,12 @@ rollbackgameengine.components.spritemap = {
 
 		//detect already animating
 		if(this.spritemapAnimation === array) {
+            //start a stopped animation
+            if(this._spritemapAnimationPosition < 0) {
+                this._spritemapAnimationPosition = 0;
+            }
+            
+            //return
 			return;
 		}
 
