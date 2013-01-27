883e0e52741514010a13c91d905a257a7a4352bc
N A <xxmy_emailxx@yahoo.com>
2013-01-26 22:13:03 -0500

    Fixed spacing

--- shared/rollbackgameengine/networking/message.js
+++ shared/rollbackgameengine/networking/message.js
@@ -6,39 +6,39 @@
 rollbackgameengine.networking.messageBitSize = 8;
 rollbackgameengine.networking.variableLengthEncodeBitSize = 7;
 rollbackgameengine.networking.calculateUnsignedIntegerBitSize = function(num) {
-    //declare variables
-    var compareValue = 1;
-    var frameBitSize = 0;
+	//declare variables
+	var compareValue = 1;
+	var frameBitSize = 0;
 
-    //integer cast
-    num = ~~(num);
+	//integer cast
+	num = ~~(num);
 
-    //normalize int
-    if(num < 0) {
-        num *= -1;
-    }
+	//normalize int
+	if(num < 0) {
+		num *= -1;
+	}
 
-    //loop
-    while(num >= compareValue) {
-        //increase compare value
-        compareValue *= 2;
+	//loop
+	while(num >= compareValue) {
+		//increase compare value
+		compareValue *= 2;
 
-        //increment bit size
-        frameBitSize++;
-    }
+		//increment bit size
+		frameBitSize++;
+	}
 
-    //0 check
-    if(frameBitSize == 0) {
-        frameBitSize++;
-    }
+	//0 check
+	if(frameBitSize == 0) {
+		frameBitSize++;
+	}
 
-    //return
-    return frameBitSize;
+	//return
+	return frameBitSize;
 };
 rollbackgameengine.networking.calculateVariableLengthUnsignedIntegerBitSize = function(num) {
-    //initial bit size
-    var bitSize = rollbackgameengine.networking.calculateUnsignedIntegerBitSize(num);
-    
-    //return calculated
-    return Math.ceil(bitSize/rollbackgameengine.networking.variableLengthEncodeBitSize) * (rollbackgameengine.networking.variableLengthEncodeBitSize+1);
+	//initial bit size
+	var bitSize = rollbackgameengine.networking.calculateUnsignedIntegerBitSize(num);
+	
+	//return calculated
+	return Math.ceil(bitSize/rollbackgameengine.networking.variableLengthEncodeBitSize) * (rollbackgameengine.networking.variableLengthEncodeBitSize+1);
 };
